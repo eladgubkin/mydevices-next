a8de93bf3f77e1eeb1b74b3df176862d44dc6fa2
Elad Gubkin <Eladox3@gmail.com>
2024-09-07 14:37:03 +0300

    add basic shadcn table

---
+++ src/app/devices/page.tsx
@@ -0,0 +1,24 @@
+import { Device, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Device[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+export default async function Devices() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
